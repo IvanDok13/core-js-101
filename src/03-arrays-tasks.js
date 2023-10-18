94d4c60008ca5291af879d525b0e50abbd3692d1
Ivan Klochkov <klochkov.ivan.sergeevich@gmail.com>
2023-10-18 14:31:22 +0300

    feat: add solution for some tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -185,10 +185,10 @@ function insertItem(arr, item, index) {
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  arr.splice(n, arr.length);
+  return arr;
 }
-
 
 /**
  * Returns the n last items of the specified array
@@ -200,10 +200,10 @@ function getHead(/* arr, n */) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  const rm = arr.splice(arr.length - n, arr.length);
+  return rm;
 }
-
 
 /**
  * Returns CSV representation of two-dimensional numeric array.
@@ -225,8 +225,12 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  const copy = [];
+  arr.map((el) => {
+    return ...el;
+  });
+  return copy;
 }
 
 /**
@@ -240,8 +244,13 @@ function toCsvText(/* arr */) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  const copy = [];
+  arr.map((el) => {
+    copy.push(el * el);
+    return copy;
+  });
+  return copy;
 }
 
 
